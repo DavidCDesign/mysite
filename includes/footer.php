fccce9ec60ec3ebaa7bcc336bae33cd7e058b60e
DavidShopStable <david@shopstable.com>
2026-01-07 16:00:11 -0800

    update for captcha
    update for captcha script location to above script.js

--- includes/footer.php
+++ includes/footer.php
@@ -63,11 +63,11 @@ if (!isset($additionalScripts)) {
     </footer>
 
     <!-- JavaScript -->
+    <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
     <script src="<?php echo url('/js/script.js'); ?>"></script>
     <script type="text/javascript"
         src="https://cdn.jsdelivr.net/npm/@emailjs/browser@4/dist/email.min.js">
     </script>
-    <script src="https://www.google.com/recaptcha/enterprise.js" async defer></script>
     <script type="text/javascript">
     (function(){
         emailjs.init({
